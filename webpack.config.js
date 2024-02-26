c08c591f773c6f6a149090a420cf14f779982b51
Filianoree <byaku9@gmail.com>
2024-02-26 21:07:24 +0100

    correzione per esecuzione locale tramite webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,19 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+  },
 };
